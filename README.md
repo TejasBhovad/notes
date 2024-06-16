a8ad927de3e633e779465e4adccde35aafdead74
TejasBhovad <tejasbhovad@gmail.com>
2024-06-17 00:58:20 +0530

    feat: added drizzle schema and DB

--- README.md
+++ README.md
@@ -6,6 +6,32 @@ Simple WebApp to distribute notes among students
 
 1.  NextJS
 2.  ShadCN UI
-3.  ... more to come
+3.  Drizzle ORM
+4.  Neon DB
 
 > Project Docuementation: https://tejasbhovad.notion.site/Notes-Site-d7245491911a4859bbe2d31eb2a4dd23?pvs=4
+
+# Todo (Testng DB):
+
+## Adding Stuff
+
+- check adding user
+- check adding subjects
+- check adding references within subjects
+- check adding folders within subjects
+- check adding notes within folders
+
+## Querying Stuff
+
+- query user with all notes uploaded
+- query subjects with all reference ids, folder ids
+- query all references
+- query all folders
+
+# Commands
+
+`pnpm drizzle-kit generate`
+
+`pnpm drizzle-kit push`
+
+`pnpm drizzle-kit studio`
---
+++ src/schema.js
@@ -0,0 +1,107 @@
+import {
+  pgTable,
+  serial,
+  text,
+  integer,
+  boolean,
+  primaryKey,
+  json,
+} from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+
+export const users = pgTable("users", {
+  id: serial("id").primaryKey(),
+  name: text("name"),
+  image: text("image"),
+  email: text("email").notNull().unique(),
+  role: text("role").default("user"),
+  subscribed_to: json("subscribed_to").default([]),
+  created_at: text("created_at").default("now()"),
+  updated_at: text("updated_at").default("now()"),
+});
+
+export const subjects = pgTable("subjects", {
+  id: serial("id").primaryKey(),
+  name: text("name").notNull().unique(),
+  archived: boolean("archived").default(false),
+  description: text("description"),
+  created_by: integer("created_by").references(() => users.id, {
+    onDelete: "no action",
+  }), // Added created_by field
+  created_at: text("created_at").default("now()"),
+  updated_at: text("updated_at").default("now()"),
+});
+
+export const references = pgTable("references", {
+  id: serial("id").primaryKey(),
+  name: text("name").notNull(),
+  subject_id: integer("subject_id").references(() => subjects.id, {
+    onDelete: "cascade",
+  }),
+  type: text("type").notNull().default("link"),
+  url: text("url").notNull(),
+  created_at: text("created_at").default("now()"),
+  updated_at: text("updated_at").default("now()"),
+  user_id: integer("user_id").references(() => users.id),
+});
+
+export const folders = pgTable("folders", {
+  id: serial("id").primaryKey(),
+  name: text("name").notNull(),
+  subject_id: integer("subject_id").references(() => subjects.id),
+  user_id: integer("user_id").references(() => users.id),
+  created_at: text("created_at").default("now()"),
+  updated_at: text("updated_at").default("now()"),
+});
+
+export const notes = pgTable("notes", {
+  id: serial("id").primaryKey(),
+  user_id: integer("user_id").references(() => users.id),
+  name: text("name").notNull(),
+  folder_id: integer("folder_id").references(() => folders.id, {
+    onDelete: "cascade",
+  }),
+  include_global: boolean("include_global").default(false),
+  created_at: text("created_at").default("now()"),
+  updated_at: text("updated_at").default("now()"),
+});
+
+// Relations
+export const usersRelations = relations(users, ({ many }) => ({
+  folders: many(folders),
+  notes: many(notes),
+  subjects: many(subjects),
+  references: many(references),
+}));
+
+export const subjectsRelations = relations(subjects, ({ many, one }) => ({
+  folders: many(folders),
+  references: many(references),
+  createdBy: one(users, {
+    fields: [subjects.created_by],
+    references: [users.id],
+  }), // Relation for created_by field
+}));
+
+export const foldersRelations = relations(folders, ({ one, many }) => ({
+  subject: one(subjects, {
+    fields: [folders.subject_id],
+    references: [subjects.id],
+  }),
+  user: one(users, { fields: [folders.user_id], references: [users.id] }),
+  notes: many(notes),
+}));
+
+export const referencesRelations = relations(references, ({ one }) => ({
+  subject: one(subjects, {
+    fields: [references.subject_id],
+    references: [subjects.id],
+  }),
+  // user can create multiple references
+  user: one(users, { fields: [references.user_id], references: [users.id] }),
+}));
+
+export const notesRelations = relations(notes, ({ one }) => ({
+  user: one(users, { fields: [notes.user_id], references: [users.id] }),
+  folder: one(folders, { fields: [notes.folder_id], references: [folders.id] }),
+}));
